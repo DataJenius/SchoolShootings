be780bd22dde6be92984848c55513e0589051158
Josh Pause <josh@pineapplestreetstudios.com>
2018-03-09 17:18:10 -0800

    Josh updated script to use new shooting data

--- 001_analyze_school_shootings.R
+++ 001_analyze_school_shootings.R
@@ -88,10 +88,11 @@ plot_summarize_data_by_feature <- function(df,selected_feature, label, title, in
     xlab(label) +
     ylab("Total") +
     labs( title = title,
-          subtitle = "All school shootings: July 26, 1764 through March 2, 2018" 
+          subtitle = "All school shootings: July 26, 1764 through March 7, 2018" 
     ) +
     theme(plot.title = element_text(hjust = 0.5),
-          plot.subtitle = element_text(hjust = 0.5))
+          plot.subtitle = element_text(hjust = 0.5)) +
+    geom_hline(yintercept=mean(df$incidents), linetype="dotted")  
 }
 
 #####################################################################################
@@ -105,7 +106,7 @@ plot_summarize_data_by_feature_per_incident <- function(df,selected_feature, lab
     xlab(label) +
     ylab("Deaths & Injuries per Incident") +
     labs( title = title,
-          subtitle = "All school shootings: July 26, 1764 through March 2, 2018" 
+          subtitle = "All school shootings: January 1, 1950 through March 7, 2018" 
     ) +
     theme(plot.title = element_text(hjust = 0.5),
           plot.subtitle = element_text(hjust = 0.5))+
@@ -113,7 +114,8 @@ plot_summarize_data_by_feature_per_incident <- function(df,selected_feature, lab
       name="Per Incident",
       values=c("#ba0823", "#ba7807","#ae08ba"),
       breaks=c("combined.per.incident", "deaths.per.incident", "injuries.per.incident"),
-      labels=c("combined", "deaths", "injuries"))
+      labels=c("combined", "deaths", "injuries")) +
+    geom_hline(yintercept=mean(df$combined.per.incident), linetype="dotted")    
 }
 
 
@@ -140,6 +142,9 @@ plot_summarize_data_by_feature_per_incident(by.state, "state", "State", "Per Inc
 # Same graph as above but with only the incidents in blue
 plot_summarize_data_by_feature(by.state,"state", "State", "School Shootings by State", TRUE)
 
+# Ignore the Wyoming outlier
+plot_summarize_data_by_feature_per_incident(by.state[by.state$state!="Wyoming",], "state", "State", "Per Incident by State")
+
 ###########################################################################
 # Look at this data by decade
 by.decade <- summarize_data_by_feature(wiki.df,"decade")
@@ -152,12 +157,6 @@ plot_summarize_data_by_feature_per_incident(by.decade, "decade", "10 Year Period
 by.decade.1950 <- summarize_data_by_feature(wiki.df[wiki.df$year >= 1950,],"decade")
 plot_summarize_data_by_feature(by.decade.1950,"decade", "10 Year Periods", "School Shootings by Decade Since 1950")
 plot_summarize_data_by_feature_per_incident(by.decade.1950, "decade", "10 Year Periods", "Per Incident by Decade Since 1950")
-
-###########################################################################
-# Look at this data by year - since 1950
-by.year.1950 <- summarize_data_by_feature(wiki.df[wiki.df$year >= 1950,],"year")
-plot_summarize_data_by_feature(by.year.1950, "year", "Individual Years", "School Shootings by Year Since 1950")
-plot_summarize_data_by_feature_per_incident(by.year.1950, "year", "Individual Years", "Per Incident by Year Since 1950")
 
 
 ###########################################################################
@@ -175,7 +174,7 @@ wiki.series <- wiki.df %>%
   )
 
 # Create a full series for every day in this period
-full.series <- data.frame(date=as.character(seq(as.POSIXct("1764-07-26"),as.POSIXct("2018-03-03"),by="days")))
+full.series <- data.frame(date=as.character(seq(as.POSIXct("1764-07-26"),as.POSIXct("2018-03-08"),by="days")))
 full.series$date <- as.character(substr(full.series$date, 1, 10))
 
 # merge wiki data into the full series
@@ -209,7 +208,7 @@ plot.incidents.xts <- incidents.xts["1950/2019"]
 plot.incidents.xts <- subset(plot.incidents.xts, select = -c(incidents))
 chart.TimeSeries(plot.incidents.xts,
                  colorset = c("darkorange","darkred","darkblue"),
-                 main = "School Shootings per Year, Rolling Average",
+                 main = "School Shootings per Year, Moving Average",
                  lwd = 2,
                  legend.loc = "topleft")
 
@@ -248,7 +247,7 @@ combined.xts$diMA5yr <- combined.xts$cMA5yr/combined.xts$MA5yr
 plot.combined.xts <- combined.xts["1950/2019", c("diMA25yr","diMA10yr","diMA5yr")]
 chart.TimeSeries(plot.combined.xts,
                  colorset = c("darkorange","darkred","darkblue"),
-                 main = "Deaths & Injuries per Incident, Rolling Average",
+                 main = "Deaths & Injuries per Incident, Moving Average",
                  lwd = 2,
                  legend.loc = "topleft")
 
@@ -256,9 +255,10 @@ chart.TimeSeries(plot.combined.xts,
 
 # let's unpack it further and graph the most horrible events - quick and dirty here
 jojo <- wiki.df
-jojo$combined <- jojo$deaths+jojo$injuries
 jojo <- jojo[jojo$year >= 1950,]
-jojo <- head(jojo[order(jojo$combined,decreasing = TRUE),],n=25)
+jojo$combined <- jojo$deaths+jojo$injuries
+jojo$extreme <- ifelse(jojo$combined >= 10,1,0)
+jojo <- jojo[jojo$extreme == 1,]
 jojo <- jojo[order(jojo$year),]
 ggplot(jojo, aes(x=date,y=combined, fill=decade)) + 
   geom_bar(stat="identity") +
@@ -266,8 +266,8 @@ ggplot(jojo, aes(x=date,y=combined, fill=decade)) +
   xlab("Date of Incident") +
   ylab("Combined Deaths & Injuries") +
   guides(fill=guide_legend(title="Year")) +
-  labs( title = "The 25 Worst School Shootings since 1950",
-        subtitle = "School shootings with the most combined deaths and injuries" 
+  labs( title = "The Worst School Shootings since 1950",
+        subtitle = "School shootings with at least 10 combined deaths and injuries" 
   ) +
   theme(plot.title = element_text(hjust = 0.5),
         plot.subtitle = element_text(hjust = 0.5))
@@ -291,8 +291,8 @@ ggplot(jojo, aes(x=decade,y=per, fill=decade)) +
   xlab("Decade") +
   scale_y_continuous("Incidents with 10+ Deaths & Injuries", labels = scales::percent) +
   guides(fill=guide_legend(title="Year")) +
-  labs( title = "Relative Frequency of Extreme Incidents",
-        subtitle = "Extreme incidents as a percentage of all school shootings by decade" 
+  labs( title = "Relative Frequency of Worst Incidents",
+        subtitle = "Worst incidents as a percentage of all school shootings by decade" 
   ) +
   theme(plot.title = element_text(hjust = 0.5),
         plot.subtitle = element_text(hjust = 0.5))
@@ -349,16 +349,17 @@ homicide.df <- read.csv("data/Wikipedia_states_by_homicide_rate_2018-03-05.csv")
 state.grades <- merge(state.grades, homicide.df, by = c("state"))
 
 # Assign each letter grade a numeric value for the sake of calculating correlation
-state.grades[state.grades$grade=="A",c("grade.score")] <- 9
-state.grades[state.grades$grade=="A-",c("grade.score")] <- 8
-state.grades[state.grades$grade=="B+",c("grade.score")] <- 7
-state.grades[state.grades$grade=="B",c("grade.score")] <- 6
-state.grades[state.grades$grade=="C+",c("grade.score")] <- 5
-state.grades[state.grades$grade=="C",c("grade.score")] <- 4
-state.grades[state.grades$grade=="C-",c("grade.score")] <- 3
-state.grades[state.grades$grade=="D",c("grade.score")] <- 2
-state.grades[state.grades$grade=="D-",c("grade.score")] <- 1
-state.grades[state.grades$grade=="F",c("grade.score")] <- 0
+# https://en.wikipedia.org/wiki/Academic_grading_in_the_United_States
+state.grades[state.grades$grade=="A",c("grade.score")] <- 96
+state.grades[state.grades$grade=="A-",c("grade.score")] <- 92
+state.grades[state.grades$grade=="B+",c("grade.score")] <- 89
+state.grades[state.grades$grade=="B",c("grade.score")] <- 86
+state.grades[state.grades$grade=="C+",c("grade.score")] <- 82
+state.grades[state.grades$grade=="C",c("grade.score")] <- 79
+state.grades[state.grades$grade=="C-",c("grade.score")] <- 72
+state.grades[state.grades$grade=="D",c("grade.score")] <- 66
+state.grades[state.grades$grade=="D-",c("grade.score")] <- 62
+state.grades[state.grades$grade=="F",c("grade.score")] <- 59
 
 # Group grades into three categories
 state.grades[state.grades$grade=="A",c("grade.group")] <- "A-B"
@@ -386,7 +387,7 @@ cor(state.grades$grade.score,state.grades$homicide.rate, method="pearson")
 ggplot(state.grades, aes(x=grade.score, y=homicide.rate))+
   geom_point() + 
   ylab("Homicides per 100k population (2016)") +
-  scale_x_continuous("Giffords Gun Score (2017)", breaks=c(0,2,4,6,9), labels=c("F","D","C","B","A")) + 
+  scale_x_continuous("Giffords Gun Score (2017)", breaks=c(59,62,66,72,79,82,86,89,92,96), labels=c("F","D-","D","C-","C","C+","B","B+","A-","A")) + 
   labs( title = "Homicides v Giffords Gun Score",
         subtitle = paste("All 50 States by Score, Pearson Correlation Coeffecient: ",round(cor(state.grades$grade.score,state.grades$homicide.rate, method="pearson"),4), sep="")) +
   theme(plot.title = element_text(hjust = 0.5),
@@ -419,11 +420,11 @@ state.grades[is.na(state.grades$combined.per.100k),c("combined.per.100k")] <- st
 state.grades
 
 
-# Incidents v Giffords Gun Score: -0.3028 as grade improves  (since 1980)
+# Incidents v Giffords Gun Score
 ggplot(state.grades, aes(x=grade.score, y=incidents.per.100k))+
   geom_point() + 
   ylab("Incidents per 100k population (2017)") +
-  scale_x_continuous("Giffords Gun Score (2017)", breaks=c(0,2,4,6,9), labels=c("F","D","C","B","A")) + 
+  scale_x_continuous("Giffords Gun Score (2017)", breaks=c(59,62,66,72,79,82,86,89,92,96), labels=c("F","D-","D","C-","C","C+","B","B+","A-","A")) + 
   labs( title = "School Shootings (Since 2000) v Giffords Gun Score",
         subtitle = paste("All 50 States by Score, Pearson Correlation Coeffecient: ",round(cor(state.grades$grade.score,state.grades$incidents.per.100k, method="pearson"),4), sep="")) +
   theme(plot.title = element_text(hjust = 0.5),
@@ -440,19 +441,24 @@ ggplot(state.grades, aes(x=grade.group, y=incidents.per.100k))+
   theme(plot.title = element_text(hjust = 0.5),
         plot.subtitle = element_text(hjust = 0.5))
 
+# quick counts
+nrow(state.grades[state.grades$incidents==0,c("state","incidents","grade")])
+nrow(state.grades[state.grades$incidents>0,c("state","incidents","grade")])
+jojo <- state.grades[state.grades$incidents==0,c("state","incidents","grade")]
+state.grades[order(state.grades$incidents, decreasing=TRUE),c("state","incidents","grade")]
 
-# D&I v Giffords Gun Score: -0.1113 as grade improves (since 1980)
+# D&I v Giffords Gun Score:
 ggplot(state.grades, aes(x=grade.score, y=combined.per.100k))+
   geom_point() + 
   ylab("Deaths & injuries per 100k population (2017)") +
-  scale_x_continuous("Giffords Gun Score (2017)", breaks=c(0,2,4,6,9), labels=c("F","D","C","B","A")) + 
+  scale_x_continuous("Giffords Gun Score (2017)", breaks=c(59,62,66,72,79,82,86,89,92,96), labels=c("F","D-","D","C-","C","C+","B","B+","A-","A")) + 
   labs( title = "School Shootings (Since 2000) v Giffords Gun Score",
         subtitle = paste("All 50 States by Score, Pearson Correlation Coeffecient: ",round(cor(state.grades$grade.score,state.grades$combined.per.100k, method="pearson"),4), sep="")) +
   theme(plot.title = element_text(hjust = 0.5),
         plot.subtitle = element_text(hjust = 0.5)) +
   geom_smooth(method='lm')
 
-# D&I v Giffords Gun Score: -0.1113 as grade improves
+# D&I v Giffords Gun Score:
 # We have an outlier - Wyoming - 1986
 # https://en.wikipedia.org/wiki/Cokeville_Elementary_School_hostage_crisis
 
@@ -466,3 +472,5 @@ ggplot(state.grades, aes(x=grade.group, y=combined.per.100k))+
   theme(plot.title = element_text(hjust = 0.5),
         plot.subtitle = element_text(hjust = 0.5))
 
+
+state.grades[order(state.grades$combined.per.100k, decreasing=TRUE),c("state","combined.per.100k","grade")]
